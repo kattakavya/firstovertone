44c15487c675e76d73a8fb3892f0b79b1d925a1b
kattakavya <kavya@front.live>
2019-02-27 20:18:24 +0530

    need to be change

---
+++ src/firstovertone/core.clj
@@ -0,0 +1,15 @@
+(ns firstovertone.core
+  (:use [overtone.live]
+   [overtone.inst.piano]
+   [tenor.constructs]
+   [tenor.melody]
+   [tenor.harmony]))
+
+(defn play-note-with-instrument [instrument note]
+  (instrument note))
+
+(defn play-chord-with-instrument [instrument chord]
+  (doseq [note chord]
+    (play-note-with-instrument instrument note)))
+
+
---
+++ test/firstovertone/core_test.clj
@@ -0,0 +1,7 @@
+(ns firstovertone.core-test
+  (:require [clojure.test :refer :all]
+            [firstovertone.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
